0d8b3498d8c56a95c53509225ed6833c9f3f90e9
Sheep-heee <sheephi0609@gmail.com>
2025-09-02 14:21:32 +0900

    supabase setting

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import {
+  createServerClient,
+  type CookieMethodsServer,
+} from "@supabase/ssr";
+
+export const supabaseServer = async () => {
+  const cookieStore = await cookies();
+
+  const cookieMethods: CookieMethodsServer = {
+    getAll() {
+      return cookieStore.getAll();
+    },
+    setAll(cookiesToSet) {
+      cookiesToSet.forEach(({ name, value, options }) => {
+        cookieStore.set(name, value, options);
+      });
+    },
+  };
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies: cookieMethods }
+  );
+};
